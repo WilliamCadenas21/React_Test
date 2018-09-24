7be552ea155491ea4f7a4c92d005b0ab03442f44
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2018-09-23 21:48:41 -0500

    Project inti

---
+++ Homepage.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+export default class Homeoage extends Component{
+	render(){
+		return (
+			<View style={styles.wrapper}>
+				<View style={styles.tittleWrapper}>
+					<Text style={styles.title}>Carpool BAQ</Text>
+				</View>
+				<View>
+					<Text style={styles.subtitle}>Powered by CUENKLEITOR</Text>
+				</View>
+			</View>
+		);
+	}
+}
+const styles = StyleSheet.create({
+	wrapper:{
+		backgroundColor:'#f1c40f',
+		flex:1,
+		justifyContent:'center',
+		alignItems:'center'
+	},
+	title:{
+		color:'white',
+		fontSize:35,
+		fontWeight:'bold'
+	},
+	subtitle:{
+		color:'white',
+		fontWeight:'200'
+	},
+	tittleWrapper:{
+		justifyContent:'center',
+		flex:1
+	}
+
+});
